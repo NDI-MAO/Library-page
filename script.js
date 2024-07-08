88263ddce44a5c48bc742d0f147ed397e8a25a16
Rugwiza <drugwiza@gmail.com>
2024-07-08 10:20:40 +0200

    added A layout and working to fix the js to make it functional

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+ function myFunction() {
+    let x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+x.style.display = "block";
+} else {
+     x.style.display = "none";}}document.addEventListener('DOMContentLoaded', function() {let x = document.getElementById("myDIV");
+x.style.display = 'none';
+}
+);
+
 const myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -5,8 +15,23 @@ this.title = title;
 this.author = author;
 this.pages = pages;
 this.read= read;
-this.info = function(){
-    console.log(this.name,',',this.author,',',this.pages,',',this.read)
 }
+function addBook(event) {
 
-  }
+//form data
+let title = document.getElementById('bName').value;
+let author = document.getElementById('aName').value;
+let pages = document.getElementById('bPages').value;
+let read = document.getElementById('bRead').value;
+
+//Table data
+const nameRow = document.getElementById('nameRow');
+const authorRow = document.getElementById('authorRow');
+const pageRow = document.getElementById('pageRow');
+const readRow = document.getElementById('readRow');
+
+nameRow.innerHTML = title;
+authorRow.innerHTML = author;
+pageRow.innerHTML = pages;
+readRow.innerHTML = read;
+}
