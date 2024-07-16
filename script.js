349b10c08a95ea9b72e949a2084a992c482ea707
Rugwiza <drugwiza@gmail.com>
2024-07-16 09:41:01 +0200

    fixed minor error on HTML tag name

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function myFunction() {
  });
 
  class Book {
-     constructor(title, author, pages, read) {
+    constructor(title, author, pages, read) {
          this.title = title;
          this.author = author;
          this.pages = pages;
          this.read = read;
-     }
+    }
  }
 
  const myLibrary = [];
