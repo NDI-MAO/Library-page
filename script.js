19461c6b131657c983643dd392bf76c0453e5209
Rugwiza <drugwiza@gmail.com>
2024-07-16 13:46:17 +0200

    updated read option to toggle to not read

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function myFunction() {
      let title = document.getElementById('bName').value;
      let author = document.getElementById('aName').value;
      let pages = document.getElementById('bPages').value;
-     let read = document.getElementById('bRead').value;
+     let read = document.getElementById('bRead').value === 'yes'?'Read':'Not Read';
  
      // Create a new book object and add to the library
      const newBook = new Book(title, author, pages, read);
@@ -46,19 +46,29 @@ function myFunction() {
      nameCell.innerHTML = title;
      authorCell.innerHTML = author;
      pagesCell.innerHTML = pages;
-     readCell.innerHTML = read;
+     //readCell.innerHTML = read;
+     readCell.innerHTML= `<button onclick="readOrNot(this)">${read}</button>`;
      deleteCell.innerHTML = '<button onclick="deleteRow(this)">×</button>';
  
-     // Clear form
+     // Clear forms
      document.getElementById('bName').value = "";
      document.getElementById('aName').value = "";
      document.getElementById('bPages').value = "";
      document.getElementById('bRead').value = "";
  }
+     //Function to swap read and not read
+     function readOrNot(button){
+        const bookStatus = button.textContent;
+        if(bookStatus ==='Read'){
+        button.textContent='Not Read';
+        }else{
+            button.textContent ='Read';
+        }
+
+     }
  
  // Function to delete a row
  function deleteRow(button) {
      const row = button.parentElement.parentElement;
      row.remove();
  }
- 
